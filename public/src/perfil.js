d2cf29db569d9d965299bad2e4fd50f5df44f467
Francisco <frankiiize.gmail.com>
2021-08-04 06:28:37 -0400

    cambios en public para resolver bug

--- public/src/perfil.js
+++ public/src/perfil.js
@@ -436,6 +436,7 @@ function selectOptionMascotaDelete(){
     return selecMascotaDelete.value
 }
 let getMascotas = () => {
+    debugger
     firebase.auth().onAuthStateChanged((userloged) => {
         const user = firebase.auth().currentUser; 
         //console.log(user)
